4c4abe1b8063d365d5d1b908fdad343a1fe6840f
shkarman12 <shaikharman79000@gmail.com>
2026-02-27 07:25:53 +0530

    NEW REPO

---
+++ jenkinsfile.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    environment {
+        NAME = "Arman"
+        COURSE = "DevOps"
+        FIELD = "IT"
+        HOME = "/tmp/"
+    }
+
+    stages {
+        stage('Print Variables') {
+            steps {
+                echo "My name is ${NAME}"
+                echo "I am learning ${COURSE}"
+                echo "My Field is ${FIELD}"
+            }
+        }
+        stage('Listing Directory') {
+            steps{
+                sh 'ls ${HOME}'
+            }
+            
+        }
+    }
+}
